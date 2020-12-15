8588b1b56d9eb316ba8284c77496ba7e08f9229c
Yaroslav Abrasimov <yabrasimov94@gmail.com>
2020-12-15 23:15:18 +0200

    fix: remove demo files | fix file structure

--- src/js/demo.js
+++
@@ -1,8 +0,0 @@
-// I will delete this. Added to show how modules works
-export default function demo() {
-  const div = document.createElement("div");
-  div.style =
-    "width: 300px; height: 300px; background: url(./img/favicon.svg);";
-  div.innerText = "This div displayed by demo.js module";
-  document.body.append(div);
-}
--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
     new CopyPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, "./src/html"),
+          from: path.resolve(__dirname, "./src/index.html"),
           to: path.resolve(__dirname, "./dist"),
         },
         {
