21fe75fecb61e728e87826a025cc31202eaa33fa
Yaroslav Abrasimov <yabrasimov94@gmail.com>
2020-12-19 13:42:50 +0200

    fix: eslint hideOtherMainElems.js || docs: add comments to this function

--- src/js/utility/hideOtherMainElems.js
+++ src/js/utility/hideOtherMainElems.js
@@ -1,12 +1,21 @@
+// function hide all main child elemnts except one, passed as arg
+// self invoking to avoid unnesessary code in module later, need only returned func
 export default (function () {
+  // closure used to set timeout on toggle fullscreen if app in fullscreen mode
   let currentMode = "visible";
 
   return function (notHiddenElem) {
-    console.log(notHiddenElem)
+    // get main children
     const main = document.querySelector("main");
     const mainElements = Array.from(main.children);
+
+    // set timeout time depends on closure statement
     const timeout = currentMode === "visible" ? 0 : 300;
+
+    // change closure statement value
     currentMode = timeout === 0 ? "hidden" : "visible";
+
+    // hide elements by toggle class
     mainElements.map((e) => {
       if (e !== notHiddenElem) {
         setTimeout(() => {
@@ -15,5 +24,5 @@ export default (function () {
       }
       return null;
     });
-  }
+  };
 })();
