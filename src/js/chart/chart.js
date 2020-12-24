cb385e570b5a61ddebd6140a76c33b6dc8b26780
Palyka Dima <polykodima@mail.ru>
2020-12-24 04:37:47 +0300

    feat: finish 100 000

--- src/js/chart/chart.js
+++ src/js/chart/chart.js
@@ -1,4 +1,6 @@
 import Chart from "../../../node_modules/chart.js/dist/Chart.min";
+import properties from "../properties";
+
 
 export default function (stateControls, region, period, value) {
   let chart;
@@ -413,8 +415,7 @@ if (value === 'absolute' || value === undefined) {
       let countryObj = properties.apiDataRelative.find((e) => {
         if (e.Country === region) return e;
        });
-      population = countryObj.population;
-      console.log(population);
+      let population = countryObj.population;
       fetch(`https://disease.sh/v3/covid-19/historical/${region}?lastdays=all`) // for specific country
         .then((response) => response.json())
         .then((data) => {
@@ -422,35 +423,35 @@ if (value === 'absolute' || value === undefined) {
           let keys = Object.keys(data.timeline.cases);
           for (let i = 0, l = keys.length; i < l; i += 1) {
             arrDates.push(keys[i]);
-            arrTotalCases.push(data.timeline.cases[keys[i]]);
+            arrTotalCases.push((data.timeline.cases[keys[i]]*100000)/population);
           }
           keys = Object.keys(data.timeline.deaths);
           for (let i = 0, l = keys.length; i < l; i += 1) {
             // arrDates.push(keys[i]);
-            arrTotalDeaths.push(data.timeline.deaths[keys[i]]);
+            arrTotalDeaths.push((data.timeline.deaths[keys[i]]*100000)/population);
           }
 
           keys = Object.keys(data.timeline.recovered);
           for (let i = 0, l = keys.length; i < l; i += 1) {
             // arrDates.push(keys[i]);
-            arrTotalRecovered.push(data.timeline.recovered[keys[i]]);
+            arrTotalRecovered.push((data.timeline.recovered[keys[i]]*100000)/population);
           }
 
           if (stateControls === "cases") {
             state = arrTotalCases;
-            title = `total cases ${region} summary`;
+            title = `total cases ${region} summary per 100 000`;
             color = 'rgba(255, 211, 0, 0.8)';
           } else if (stateControls === "deaths") {
             state = arrTotalDeaths;
-            title = `total deaths ${region} summary`;
+            title = `total deaths ${region} summary per 100 000`;
             color = 'rgba(124, 10, 10, 0.8)';
           } else if (stateControls === "recovered") {
             state = arrTotalRecovered;
-            title = `total recovered ${region} summary`;
+            title = `total recovered ${region} summary per 100 000`;
             color = 'rgba(57, 137, 48, 0.8)';
           } else if (stateControls === undefined) {
             state = arrTotalCases;
-            title = `total cases ${region} summary`;
+            title = `total cases ${region} summary per 100 000`;
             color = 'rgba(255, 211, 0, 0.8)';
 
           }
@@ -488,6 +489,7 @@ if (value === 'absolute' || value === undefined) {
     }
   } else {
     if (region === undefined || region === "World") {
+      let population = 7830458560;
       fetch("https://covid19-api.org/api/timeline") // for all world
         .then((response) => response.json())
         .then((data) => {
@@ -498,42 +500,42 @@ if (value === 'absolute' || value === undefined) {
           for (let i = data.length - 1; i > 0; i -= 1) {
             // get arr of total cases
             arrTotalCases.push(
-              data[i - 1].total_cases - data[i].total_cases < 0
+              ((data[i - 1].total_cases - data[i].total_cases < 0
                 ? 0
-                : data[i - 1].total_cases - data[i].total_cases
+                : data[i - 1].total_cases - data[i].total_cases)*100000)/population
             );
           }
           for (let i = data.length - 1; i > 0; i -= 1) {
             // get arr of total deaths
             arrTotalDeaths.push(
-              data[i - 1].total_deaths - data[i].total_deaths < 0
+              ((data[i - 1].total_deaths - data[i].total_deaths < 0
                 ? 0
-                : data[i - 1].total_deaths - data[i].total_deaths
+                : data[i - 1].total_deaths - data[i].total_deaths)*100000)/population
             );
           }
           for (let i = data.length - 1; i > 0; i -= 1) {
             // get arr of total recovered
             arrTotalRecovered.push(
-              data[i - 1].total_recovered - data[i].total_recovered < 0
+              ((data[i - 1].total_recovered - data[i].total_recovered < 0
                 ? 0
-                : data[i - 1].total_recovered - data[i].total_recovered
+                : data[i - 1].total_recovered - data[i].total_recovered)*100000)/population
             );
           }
           if (stateControls === "cases" && region === "World") {
             state = arrTotalCases;
-            title = "total historical daily cases ";
+            title = "total historical daily cases per 100 000";
             color = 'rgba(255, 211, 0, 0.8)';
           } else if (stateControls === "deaths" && region === "World") {
             state = arrTotalDeaths;
-            title = "total historical daily death";
+            title = "total historical daily death per 100 000";
             color = 'rgba(124, 10, 10, 0.8)';
           } else if (stateControls === "recovered" && region === "World") {
             state = arrTotalRecovered;
-            title = "total historical daily recovered";
+            title = "total historical daily recovered per 100 000";
             color = 'rgba(57, 137, 48, 0.8)';
           } else if (stateControls === undefined && region === undefined) {
             state = arrTotalCases;
-            title = "total historical daily cases";
+            title = "total historical daily cases per 100 000";
             color = 'rgba(255, 211, 0, 0.8)';
           }
 
@@ -570,6 +572,10 @@ if (value === 'absolute' || value === undefined) {
           chart.update();
         });
     } else {
+      let countryObj = properties.apiDataRelative.find((e) => {
+        if (e.Country === region) return e;
+       });
+      population = countryObj.population;
       fetch(`https://disease.sh/v3/covid-19/historical/${region}?lastdays=all`) // for specific country
         .then((response) => response.json())
         .then((data) => {
@@ -578,23 +584,23 @@ if (value === 'absolute' || value === undefined) {
           for (let i = 0, l = keys.length; i < l; i += 1) {
             arrDates.push(keys[i]);
             arrTotalCases.push(
-              data.timeline.cases[keys[i]] - data.timeline.cases[keys[i - 1]] <
+              ((data.timeline.cases[keys[i]] - data.timeline.cases[keys[i - 1]] <
                 0
                 ? 0
                 : data.timeline.cases[keys[i]] -
-                    data.timeline.cases[keys[i - 1]]
+                    data.timeline.cases[keys[i - 1]])*100000)/population
             );
           }
           keys = Object.keys(data.timeline.deaths);
           for (let i = 0, l = keys.length; i < l; i += 1) {
             // arrDates.push(keys[i]);
             arrTotalDeaths.push(
-              data.timeline.deaths[keys[i]] -
+              ((data.timeline.deaths[keys[i]] -
                 data.timeline.deaths[keys[i - 1]] <
                 0
                 ? 0
                 : data.timeline.deaths[keys[i]] -
-                    data.timeline.deaths[keys[i - 1]]
+                    data.timeline.deaths[keys[i - 1]])*100000)/population
             );
           }
 
@@ -602,30 +608,30 @@ if (value === 'absolute' || value === undefined) {
           for (let i = 0, l = keys.length; i < l; i += 1) {
             // arrDates.push(keys[i]);
             arrTotalRecovered.push(
-              data.timeline.recovered[keys[i]] -
+              ((data.timeline.recovered[keys[i]] -
                 data.timeline.recovered[keys[i - 1]] <
                 0
                 ? 0
                 : data.timeline.deaths[keys[i]] -
-                    data.timeline.deaths[keys[i - 1]]
+                    data.timeline.deaths[keys[i - 1]])*100000)/population
             );
           }
 
           if (stateControls === "cases") {
             state = arrTotalCases;
-            title = `historical daily cases ${region}`;
+            title = `historical daily cases ${region} per 100 000`;
             color = 'rgba(255, 211, 0, 0.8)'
           } else if (stateControls === "deaths") {
             state = arrTotalDeaths;
-            title = `historical daily deaths ${region}`;
+            title = `historical daily deaths ${region} per 100 000`;
             color = 'rgba(124, 10, 10, 0.8)'
           } else if (stateControls === "recovered") {
             state = arrTotalRecovered;
-            title = `historical daily recovered ${region}`;
+            title = `historical daily recovered ${region} per 100 000`;
             color = 'rgba(57, 137, 48, 0.8)'
           } else if (stateControls === undefined) {
             state = arrTotalCases;
-            title = `historical daily cases ${region}`;
+            title = `historical daily cases ${region} per 100 000`;
             color = 'rgba(255, 211, 0, 0.8)'
           }
 
