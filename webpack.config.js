525c833b59795bdc83e6656d08b4daadccc1d837
Yaroslav Abrasimov <yabrasimov94@gmail.com>
2021-05-07 14:04:40 +0300

    feat: update packages and change webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,35 @@
 const path = require('path');
-const webpack = require('webpack');
 const CopyPlugin = require('copy-webpack-plugin');
 const HtmlMinimizerPlugin = require('html-minimizer-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/js/app.js',
-  // devtool: "source-map",
+  entry: {
+    main: path.resolve(__dirname, './src/js/app.js'),
+  },
   watch: true,
 
   output: {
     path: path.resolve(__dirname, './dist'),
-    library: 'main',
     filename: 'bundle.js',
+    publicPath: '',
   },
 
   plugins: [
-    new webpack.ProgressPlugin(),
     new MiniCssExtractPlugin({
       filename: 'style.css',
     }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+      exclude: [/^$/],
+      filename: 'index.html',
+    }),
     new CopyPlugin({
       patterns: [
-        {
-          from: path.resolve(__dirname, './src/index.html'),
-          to: path.resolve(__dirname, './dist'),
-        },
         {
           from: path.resolve(__dirname, './assets'),
           to: path.resolve(__dirname, './dist'),
@@ -41,32 +42,20 @@ module.exports = {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        include: [path.resolve(__dirname, 'src')],
-        loader: 'babel-loader',
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
       },
       {
-        test: /.(scss|css)$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true,
-            },
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true,
-            },
-          },
-        ],
+        test: /\.(sa|sc|c)ss$/,
+        exclude: /^$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        loader: 'url-loader',
+        exclude: /^$/,
+        type: 'asset',
       },
     ],
   },
